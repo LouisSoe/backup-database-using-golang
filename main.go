779962b29b5eedee0ff7c8c187df2c218efc1949
Arallel <94737179+arallel@users.noreply.github.com>
2025-10-21 19:28:37 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("Gagal load file .env:", err)
+		return
+	}
+
+	if err := InitGlobalMinio(); err != nil {
+		fmt.Println("Gagal inisialisasi MinIO:", err)
+		return
+	}
+
+	loc, err := time.LoadLocation("Asia/Jakarta")
+	if err != nil {
+		fmt.Println("Gagal load lokasi zona waktu:", err)
+		return
+	}
+
+	ticker := time.NewTicker(12 * time.Hour)
+	defer ticker.Stop()
+
+	for {
+		now := time.Now().In(loc)
+		fmt.Printf("Jadwal backup: %s\n", now.Format("2006-01-02 15:04:05"))
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Minute)
+		if err := BackupPostgresToS3(ctx); err != nil {
+			fmt.Println("Backup failed:", err)
+		} else {
+			fmt.Println("Backup and upload to MinIO successful.")
+		}
+		cancel()
+		<-ticker.C
+	}
+}
